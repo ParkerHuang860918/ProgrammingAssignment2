c9501cb3df053f838d11a87d0cbcef364725f3f1
ParkerHuang860918 <huangfeiyu120@hotmail.com>
2017-01-07 22:29:34 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## the two functions combined will calculate and cache the inverse of a matrix 
+## to save the computation time if the inverse has been in the cache area
+
 
 ## Write a short comment describing this function
-
+## this first function creates matrix as input and returns a list including four elements related to matrix and inverse of matrix
 makeCacheMatrix <- function(x = matrix()) {
     
     
@@ -22,7 +24,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## the second function is ti check if the inverse of matrix has been saved in the cached area, if there, return cached data, 
+## else calcualte the inverse of the new matrix
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     inv <- x$getinverse()
